bd08e93832beea2e327f494d00c8ac7fd15eba5d
Priyobrato Das <priyodas12@gmail.com>
2020-02-16 09:34:59 +0530

    initial file commit

---
+++ SpringBootActuator/src/test/java/io/workspace/SpringBootActuator/SpringBootActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.workspace.SpringBootActuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
